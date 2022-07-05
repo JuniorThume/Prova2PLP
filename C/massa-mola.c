3062c5cbde2c3d89a487c8436266787401e778f7
Leonardo Sonco <leonardokarling@hotmail.com>
2022-07-05 09:40:43 -0300

    modificado o metdo carlos

--- C/massa-mola.c
+++ C/massa-mola.c
@@ -1,31 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define TMAX 1000000
+#define TMAX 1
 
 float massa_mola(){
-    float i=-1.0;
+    int i=-1;
     float x = -1.0;
     float vx = 0.0;
-    float vel = (float*) malloc(sizeof(float) * TMAX);
-    float pos = (float*) malloc(sizeof(float) * TMAX);
+    float dt;
+    dt = 0.001;
+    float* vel = (float*) malloc(sizeof(float) * (TMAX/dt));
+    float* pos = (float*) malloc(sizeof(float) * (TMAX/dt));
     float dx;
     float dvx;
-    float dt = 0.00001;
+    
     float t;
     float m = 1.0;
     float k = 1.0;
-    for (t=0; t < TMAX ;t+=dt){
+    
+    for (t=0.0; t < TMAX ;t+=dt){
         dx = vx*dt;
         dvx = -(k/m)*x*dt;
         x = x+dx;
         vx = vx+dvx;
         pos[++i]=x;
         vel[i]=vx;
+        printf("%f teste\n",t);
     }
+    
     free (vel);
     free (pos);
-
-    return time = 2.0;
+    
+    return 2.0;
 }
 
 
