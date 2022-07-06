4de03bfbe70230b46e85536a9345546775051dd6
Rodrigo Klein <rodrigoklein2010@gmail.com>
2022-07-06 15:15:06 -0300

    massa-mola em lisp (incompleto)

--- Lisp/massamola.lisp
+++ Lisp/massamola.lisp
@@ -3,23 +3,27 @@
 (defvar dt 0.001)
 (defvar x -1.0)
 (defvar vx 0.0)
-(defvar tam_vet (/ TMAX dt))
-(setf vel (make-array '(tam_vet)))
-(setf pos (make-array '(tam_vet)))
+
+;(defvar tam_vet 100000)
+;(setf tam_vet (/ TMAX dt))
+
+(setf vel (make-array '(100)))
+(setf pos (make-array '(100)))
 (defvar dx 0.0)
 (defvar dvx 0.0)
-(defvar t 0.0)
+(defvar u 0.0)
 (defvar m 1.0)
 (defvar k 1.0)
 (defvar aux 0)
 (defvar time 0)
+(defvar i 0)
 
 
 (defun massa_mola()
 
-    (loop for i from t to (< t TMAX)
-        (setf t (+ t dt))
-        (setf dvx (* (* (- (/ k m)) x) dt))
+    (loop 
+        (setf u (+ u dt))
+        (setf dvx (* (* (- 0 (/ k m)) x) dt))
         (setf vx (+ vx dvx))
         (setf dx (* vx dt))
         (setf x (+ x dx))
@@ -27,17 +31,32 @@
         (setf aux (+ i 1))
         (setf (aref pos aux) x)
         (setf (aref vel i) vx)
+
+        (setf i (+ i 1))
+
+        (when (>= u TMAX) (return 0))
         
     )
 
-    (with-open-file (output "tempo.txt" 
+    (with-open-file (output "resultados-massa-mola.txt" 
         :direction :output 
         :if-does-not-exist :create 
         :if-exists :append)
 
-        (format output  )
+        (print '(Exemplo de saida) output)
     )
     
 )
 
+(massa_mola)
+
 (setf time (get-internal-run-time))
+    
+(with-open-file (output "runtime-massa-mola.txt" 
+    :direction :output 
+    :if-does-not-exist :create 
+    :if-exists :append)
+
+    (format output time)
+)
+
