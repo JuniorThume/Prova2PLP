7da9cb51282cf0f58e9078b2f7c420360f4fa1ef
Leonardo Sonco <leonardokarling@hotmail.com>
2022-07-07 19:02:58 -0300

    Modificado monte carlo python

--- Python/monte-carlo/montecarlo.py
+++ Python/monte-carlo/montecarlo.py
@@ -1,39 +1,47 @@
 
 import random
-import math
 import time
 
-cont=0
-dentro=0
-fora=0
-
-arq = open("Python/monte-carlo/runtime-montecarlo.txt","w")
-
-inicio = time.time()
-
-while (cont<1000000): 
-
-   x=2*random.random()-1   
-   y=2*random.random()-1  
-
-   ponto = x*x + y*y             
-
-   if (ponto<=1):        
-      dentro +=1
+def monteCarlo() :
+   cont = 0
+   dentro=0
+   fora=0
+   for j in range(1000000):
+      x=2*random.random()-1   
+      y=2*random.random()-1  
       
-   else:            
-      fora +=1
-   cont += 1
+      ponto = x**2 + y**2
+      
+      if (ponto<=1):        
+         dentro +=1
+         
+      else:            
+         fora +=1
+   
+   valor_pi = 4*dentro/(dentro+fora)
+   return valor_pi
 
-print("dentro ",dentro)
-print("fora ",fora)
-valor_pi = 4*dentro/(dentro+fora)
+def main():
+  
+   arq = open("Python/monte-carlo/runtime-montecarlo.txt","w")
+   arq.write("RESULTADO MONTE CARLO PYTHON\n")
+   media = 0.0
+   
+   for i in range(10):
+      inicio = time.time()
+      valor_pi= monteCarlo()
+      fim = time.time()
 
-fim = time.time()
+      tempo = (fim-inicio)*1000 #milissegundos
+      media = media+tempo
+      print("Valor simulado de pi: ",valor_pi)
+      print("Tempo de execucao = "+ str(tempo)+"\n")
+      arq.write("Tempo de execucao = "+ str(tempo)+"\n")
 
-tempo = (fim-inicio)*1000 #milissegundos
+   media = media/10
+   
+   print("Tempo medio das execucoes "+str(media)+"\n")
+   arq.write("Tempo medio das execucoes "+str(media)+"\n")
+   arq.close()
 
-print("Valor simulado de pi: ",valor_pi)
-print("Tempo de execucao = "+ str(tempo)+"\n")
-arq.write("Tempo de execucao = "+ str(tempo)+"\n")
-
+main()
