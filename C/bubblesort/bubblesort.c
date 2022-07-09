eb5559b7781bec948154980625c8ad6a8a1958c3
Junior Thume <juninhothume21@gmail.com>
2022-07-09 18:58:21 -0300

    Recompilado bubble em C - ESTA OK

--- C/bubblesort/bubblesort.c
+++ C/bubblesort/bubblesort.c
@@ -4,9 +4,8 @@
 #include <string.h>
 #define TAM 10000
 
-int bubbleSort(FILE* arq, int qtd){
-    clock_t t;
-    t = clock();
+void bubbleSort(FILE* arq, int qtd){
+    
     int vet[TAM], aux, i, j;
        
     
@@ -14,7 +13,9 @@ int bubbleSort(FILE* arq, int qtd){
         vet[i]= rand() % 10000;
         
     }
-    
+
+    clock_t t;
+    t = clock();
     for(i=0; i < TAM - 1; i++){       
         for(j=0; j < TAM - i - 1; j++){          
             if(vet[j] > vet[j+1]){               
@@ -26,10 +27,8 @@ int bubbleSort(FILE* arq, int qtd){
         }
     }
     t = clock()-t;
-   
     fprintf(arq,"%lf \n",(((double)t/CLOCKS_PER_SEC)*1000)); // Milissegundos
     
-    return (((double)t/CLOCKS_PER_SEC)*1000);
 }
 
 
@@ -38,12 +37,9 @@ int main(){
     int i;
     double tempoMedio,tempo;
     arq = fopen("tempo-bubblesort.csv","wt");
-    for(i=0; i<1000 ; i++){
-        tempo=bubbleSort(arq,i);
-        tempoMedio = tempoMedio+tempo;     
+    for(i=0; i<100 ; i++){
+        bubbleSort(arq,i);    
     }  
-    tempoMedio = tempoMedio/1000.0;
-    
     
     fclose(arq);
     
