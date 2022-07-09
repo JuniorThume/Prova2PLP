31fc20a25d778648c4da6bd88ab009241dbab596
Junior Thume <juninhothume21@gmail.com>
2022-07-08 21:45:26 -0300

    (resultados do massa-mola - Junior) - C FINALIZADO

--- C/massa-mola/massa-mola.c
+++ C/massa-mola/massa-mola.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 #define TMAX 100.0
-#define dt 0.001
 
-void escreve_saida(float *vet, float *vet1);
 
-float massa_mola(){
+void escreve_saida(float *vet, float *vet1, float dtAtual);
+
+float massa_mola(float dt){
     clock_t time;
     time = clock();
     int i=-1;
@@ -30,16 +30,21 @@ float massa_mola(){
         vel[i]=vx;
     }
     time = clock()-time;
-    escreve_saida(vel,pos);
+    escreve_saida(vel,pos,dt);
     free (vel);
     free (pos);
     time = ((double)time/CLOCKS_PER_SEC)*1000; 
     return time;
 }
-void escreve_saida(float *vet, float *vet1){
+void escreve_saida(float *vet, float *vet1, float dtAtual){
     FILE* arq;
-    arq = fopen("resultado-massa-mola.csv","wt");
-    for(int i=0; i < (TMAX/dt); i++){
+    char *nomeArq;
+    if(dtAtual == 0.01f) {
+        arq = fopen("../../../../Resultados-MassaMola/C/runtime_massa_mola-01.csv","wt");
+    } else { 
+        arq = fopen("../../../../Resultados-MassaMola/C/runtime_massa_mola-001.csv","wt");
+    }
+    for(int i=0; i < (TMAX/dtAtual); i++){
         fprintf(arq,"%f %f\n",vet[i],vet1[i]);
     }
     fclose(arq);
@@ -48,8 +53,14 @@ void escreve_saida(float *vet, float *vet1){
 
 int main(){
     FILE* temp;
-    float runtime = massa_mola();
-    temp = fopen("runtime_massa_mola.txt","wt");
-    fprintf(temp,"Tempo de execucao %f\n", runtime);
+    float tam[2]= {0.01,0.001};
+    int l;
+    temp = fopen("runtime-massaMola.txt","wt");
+    for(l=0; l < 2; l++){
+        printf("%d\n",l);
+        float runtime = massa_mola(tam[l]);
+        fprintf(temp,"Tempo de execucao %f\n", runtime);
+        
+    }   
     fclose(temp);
 }
