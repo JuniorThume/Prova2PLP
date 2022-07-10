6048b992adf19fcefa60f962f937b611839e94fe
Junior Thume <juninhothume21@gmail.com>
2022-07-10 16:59:06 -0300

    Execuções em C FINALIZADAS - SOMENTE PEGAR RESULTADOS

--- C/massa-mola/massa-mola.c
+++ C/massa-mola/massa-mola.c
@@ -1,41 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define TMAX 1000.0f
+#define TMAX 100000.0
 
 
 void escreve_saida(float *vet, float *vet1, float dtAtual);
 
 float massa_mola(float dt){
     clock_t time;
+    
+    
+    //int tam_vet = (int)((TMAX/dt)+1);
+    
+    float *vel = (float *) malloc(10552840 * sizeof(float)); 
+    float *pos = (float *) malloc(10552840 * sizeof(float));
+    //printf("%d\n", tam_vet);
     time = clock();
     int i=0;
     float x = -1.0;
     float vx = 0.0;
-    int tam_vet = TMAX/dt;
-    float *vel = (float*) malloc(tam_vet * sizeof(float));
-    float *pos = (float*) malloc(tam_vet * sizeof(float));
+    //printf("%ld",sizeof(vel));
     float dx;
     float dvx;
     float t;
     float m = 1.0;
     float k = 1.0;
-    
-    for (t=0.0; t < TMAX ;t+=dt){
+    t=0.0; 
+    while(t < TMAX){
+        
         dvx = -(k/m)*x*dt;
         vx = vx+dvx;
         dx = vx*dt;
         x = x+dx;
-        
         pos[i]=x;
         vel[i]=vx;
-        i+=1;
+        i++;
+        t+=dt;
     }
+    
     time = clock()-time;
     escreve_saida(vel,pos,dt);
-    free (vel);
-    free (pos);
+    free(pos);
+    free(vel);
     time = ((double)time/CLOCKS_PER_SEC)*1000; 
+    
     return time;
 }
 void escreve_saida(float *vet, float *vet1, float dtAtual){
@@ -55,19 +63,9 @@ void escreve_saida(float *vet, float *vet1, float dtAtual){
 
 int main(){
     FILE* temp;
-    float tam[2];
-    tam[0] = 0.01f;
-    tam[1] = 0.001f;
-    int l;
     float runtime;
     temp = fopen("runtime-massaMola.txt","wt");
-    for(l=0; l < 2; l++){
-        printf("Passei 1\n");
-        printf("%d\n",l);
-        
-        runtime = massa_mola(tam[l]);
-        fprintf(temp,"Tempo de execucao %f\n", runtime);
-        
-    }   
+    runtime = massa_mola(0.01f);
+    fprintf(temp,"Tempo de execucao %f\n", runtime);
     fclose(temp);
 }
